03bbfd16563011de54f896fa9dbb1d540c30d5ca
Sengphet <dolongdzvch@gmail.com>
2023-10-10 11:15:53 +0700

    New API

---
+++ WebAPI/QuanLyBanHang_API/QuanLyBanHang_API/WeatherForecast.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+
+namespace QuanLyBanHang_API
+{
+    public class WeatherForecast
+    {
+        public DateTime Data { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + { int}(TemperatureC / 0.5556);
+
+        public string Summarry { get; set;}
+    }
+}
